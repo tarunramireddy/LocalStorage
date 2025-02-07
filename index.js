ec339bba0af31e519d420a9ef8618e9c9b6118e9
tarun ramireddy <tarunramireddy@gmail.com>
2025-02-06 19:48:19 -0500

    local storage check in reload

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 let saveButton = document.getElementById("saveBtn");
 let textAreaElement = document.getElementById("textplacement");
 
-saveButton.onclick = function(){
+saveButton.onclick = function () {
     let userEnteredText = textAreaElement.value;
+    console.log("Saving text to localStorage:", userEnteredText); // Debug log
     localStorage.setItem("userEnteredText", userEnteredText);
-}
+};
 
 let storedUserInputValue = localStorage.getItem("userEnteredText");
-if(storedUserInputValue === null){
+console.log("Retrieved from localStorage:", storedUserInputValue); // Debug log
+
+if (storedUserInputValue === null) {
     textAreaElement.value = "";
-}
-else{
+} else {
     textAreaElement.value = storedUserInputValue;
 }
